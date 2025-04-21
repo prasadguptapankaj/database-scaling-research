11bd93f7e4c5e48fe355e8287248581ee5e51a24
Neelesh@jobgraze.com <neelesh@jobgraze.com>
2025-04-21 10:03:05 +0530

    replica config

--- config/database.php
+++ config/database.php
@@ -64,7 +64,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'), PDO::ATTR_PERSISTENT => true,
             ]) : [],
         ],
 
